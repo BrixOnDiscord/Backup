fd58432544a32dab95297d1422e1caf1f012e249
aljonbade <baduaaldione20@gmail.com>
2024-09-19 19:28:45 +0800

    Aldione commit

---
+++ app/src/main/java/com/baguio/anunsyoapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.baguio.anunsyoapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // go to public and admin page
+        val btnGetstarted = findViewById<Button>(R.id.getstarted)
+        btnGetstarted.setOnClickListener {
+            val getstarted_Intent = Intent(this,Public_and_Admin::class.java)
+            startActivity(getstarted_Intent)
+
+        }
+
+
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/baguio/anunsyoapp/Public_and_Admin.kt
@@ -0,0 +1,17 @@
+package com.baguio.anunsyoapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Public_and_Admin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_public_and_admin)
+
+
+    }
+}
