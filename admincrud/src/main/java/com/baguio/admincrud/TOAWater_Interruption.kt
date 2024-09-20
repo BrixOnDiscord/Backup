0b9e2d1248cc4490ef4ebdcc3f6cb05c91f2f587
aljonbade <baduaaldione20@gmail.com>
2024-09-21 03:02:54 +0800

    Aldione commit

---
+++ admincrud/src/main/java/com/baguio/admincrud/TOAWater_Interruption.kt
@@ -0,0 +1,17 @@
+package com.baguio.admincrud
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TOAWater_Interruption : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_toawater_interruption)
+
+
+    }
+}
